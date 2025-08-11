d8b291019a56e87226ec9aeb3b26a4add03658fa
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-08-11 21:10:24 +0300

    test: use vitest rules runner

--- src/rules/test.ts
+++ src/rules/test.ts
@@ -1,3 +1,22 @@
+import { describe, it } from "vitest"
 import { RuleTester } from "eslint"
 
-export const ruleTester = new RuleTester()
+import type { SuiteFactory } from "vitest"
+
+type Message = string | Function
+
+class VitestRuleTester extends RuleTester {
+  static describe(message: Message, callback: SuiteFactory) {
+    describe(message, callback)
+  }
+
+  static it(message: Message, callback: SuiteFactory) {
+    it(message, callback)
+  }
+
+  static itOnly(message: Message, callback: SuiteFactory) {
+    it.only(message, callback)
+  }
+}
+
+export const ruleTester = new VitestRuleTester()
