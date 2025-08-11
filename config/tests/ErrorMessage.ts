b7295c96b615d4fcfa90d6f4cc3b825f9c68bb0d
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-08-11 21:11:20 +0300

    test: remove custom tests runner

--- config/tests/ErrorMessage.ts
+++
@@ -1,9 +0,0 @@
-export class ErrorMessage extends String {
-  constructor(private readonly message: string) {
-    super(message)
-  }
-
-  toString(): string {
-    return this.message.split("\n").slice(0, 2).join("\n")
-  }
-}
--- config/tests/run.ts
+++
@@ -1,7 +0,0 @@
-import { globSync } from "fs"
-import { Tests } from "./Tests"
-
-const paths = globSync("**/rules/**/*.test.ts")
-const tests = new Tests(paths)
-const result = await tests.toString()
-console.info(result)
