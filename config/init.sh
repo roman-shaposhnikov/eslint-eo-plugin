8b0a61f23275f86e05a5587522f1a24f5642dc03
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-08-16 14:03:23 +0300

    chore: add deps installing, tests runing and project build on init

--- config/init.sh
+++ config/init.sh
@@ -1,3 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-cp ./config/git/hooks/* ./.git/hooks
+function copyGitHooks {
+  cp ./config/git/hooks/* ./.git/hooks &&
+  echo "Client git hooks installed"
+}
+
+function installDependencies {
+  npm ci &&
+  echo "All dependencies installed"
+}
+
+function runTests {
+  npm run test &&
+  echo "All tests passed"
+}
+
+function buildProject {
+  npm run build &&
+  echo "The project builded without errors"
+}
+
+function printPartingWords {
+  echo "Happy coding!"
+}
+
+copyGitHooks &&
+installDependencies &&
+runTests &&
+buildProject &&
+printPartingWords
